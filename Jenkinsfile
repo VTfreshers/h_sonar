31a16e4cf5b1b0694b39c91b69aecfb6dcb0d16d
VTfreshers <hema970319@gmail.com>
2019-04-23 09:26:42 +0530

    committing for sonar c test

---
+++ Jenkinsfile
@@ -0,0 +1,18 @@
+pipeline
+{
+stages
+{
+stage ("fetch_code")
+{
+
+}
+stage ("code_analyse")
+{
+
+}
+stage ("testing")
+{
+
+}
+}
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	int a=10;
+	char s[10]="hemasri";
+	printf("%d is a \n",a);
+	puts(s)
+		return 0;
+}
